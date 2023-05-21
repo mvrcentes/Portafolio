39f0aca9c08b981d96b0b222b5b37c44c9cc193e
Marco Ramirez <mvrcentes@hotmail.com>
2023-05-21 11:48:24 -0600

    Add Tag component

---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+
+import style from "./Tag.module.css"
+
+export const Tag = ({ tag, children, type  = "", color = "" }) => {
+    const type_classes = type
+        ? `${style.Type} ${style[`Type__${type}`]}`
+        : `${style.Type} ${style.Gap}`
+
+    const color_classes = color
+        ? `${style.Children} ${style[`Children__${color}`]}`
+        : `${style.Children} ${style.Code}`
+
+    const children_classes = (() => {
+        switch (type) {
+            case 1:
+                return `${color_classes}`
+            case 2:
+                return `${color_classes} ${style.Margin}`
+            case 3:
+                return `${color_classes} ${style.Margin} ${style.Code}`
+        }
+    })()
+
+    const tag_classes = type
+        ? `${style.Tag} ${style[`Tag__${type}`]}`
+        : `${style.Tag}`
+
+    return (
+        <div className={type_classes}>
+            <div className={style.Tag}>{`<${tag}>`}</div>
+            <div className={children_classes}>{children}</div>
+            <div className={tag_classes}>{`</${tag}>`}</div>
+        </div>
+    )
+}
